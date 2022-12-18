881cb49ec1dab2f4b7740f5e10380661cd6c08f1
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-12-18 15:58:20 +0100

    Implement reducer, sum values and call merge phase

--- src/main/java/org/flightdata/FlightReducer.java
+++ src/main/java/org/flightdata/FlightReducer.java
@@ -1,2 +1,18 @@
-package org.flightdata;public class FlightReducer {
+package org.flightdata;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlightReducer<KEY> extends Reducer<KEY,DoubleWritable, KEY, DoubleWritable> {
+    private DoubleWritable totalBasePay = new DoubleWritable();
+
+    public void reduce(KEY key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+        double sum = 0;
+        for (DoubleWritable item: values) {
+            sum += item.get();
+        }
+        totalBasePay.set(sum);
+        context.write(key, totalBasePay);
+    }
 }
