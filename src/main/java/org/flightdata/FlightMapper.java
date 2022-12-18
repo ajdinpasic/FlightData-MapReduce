36500b5f4192df9c42ac640629bd3dc4767d44a5
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-12-18 15:57:32 +0100

    Implement mapper, read data and create key value pairs

--- src/main/java/org/flightdata/FlightMapper.java
+++ src/main/java/org/flightdata/FlightMapper.java
@@ -1,2 +1,18 @@
-package org.flightdata;public class FlightMapper {
+package org.flightdata;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+public class FlightMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+    private Text startingAiportName = new Text();
+    private DoubleWritable basePayForAirport = new DoubleWritable();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String splitRecords[] = value.toString().split(",");
+        startingAiportName.set(splitRecords[3]);
+        basePayForAirport.set(Double.parseDouble(splitRecords[12]));
+        context.write(startingAiportName, basePayForAirport); // <key-value> : <airportName-basePayForThatAiport>
+    }
 }
