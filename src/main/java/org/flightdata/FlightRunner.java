260d871b780a5caf7f334e47c555f3e6bb9653ac
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-12-18 18:08:31 +0100

    Implement runner for setting up config, classes and output

--- src/main/java/org/flightdata/FlightRunner.java
+++ src/main/java/org/flightdata/FlightRunner.java
@@ -1,11 +1,37 @@
 package org.flightdata;
-import org.apache.hadoop.mapred.JobConf;
 
-import java.io.IOException;
 
-public class FlightRunner {
-    public static void main(String[] args) throws IOException {
-        JobConf conf = new JobConf(FlightRunner.class);
-        conf.setJobName("FlightData-MapReduce");
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.w3c.dom.Text;
+
+
+public class FlightRunner extends Configured implements Tool {
+    public static void main(String[] args) throws Exception {
+        int rc = ToolRunner.run(new FlightRunner(), args);
+        System.exit(rc);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = new Job(getConf());
+        job.setJarByClass(getClass());
+        job.setJobName(getClass().getSimpleName());
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(FlightMapper.class);
+        job.setCombinerClass(FlightReducer.class);
+        job.setReducerClass(FlightReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
