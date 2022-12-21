7bc6e563d9e44378da382526455030e89d7ed864
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2022-12-21 22:45:43 +0100

    Update reducer to sum all values for each unique key

--- src/main/java/org/flightdata/FlightReducer.java
+++ src/main/java/org/flightdata/FlightReducer.java
@@ -1,18 +1,19 @@
 package org.flightdata;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.mapreduce.Reducer;
 
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
-public class FlightReducer<KEY> extends Reducer<KEY,DoubleWritable, KEY, DoubleWritable> {
-    private DoubleWritable totalBasePay = new DoubleWritable();
+public  class FlightReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
 
-    public void reduce(KEY key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-        double sum = 0;
-        for (DoubleWritable item: values) {
-            sum += item.get();
+    public void reduce(Text word, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        float sum = 0;
+        float count = 0;
+        for (FloatWritable value : values) {
+            sum += value.get();
+            count = count + 1;
         }
-        totalBasePay.set(sum);
-        context.write(key, totalBasePay);
+        context.write(word, new FloatWritable(sum));
     }
 }
